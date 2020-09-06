0dfc14040a83891105c78ca112e01a7b58c2c700
Aluno41 <aluno@dinalva41.academico.cotuca.unicamp.br>
2020-09-05 23:14:48 -0300

    estilizando as telas

---
+++ src/screens/Home/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {StyledContainer, 
+        StyledText,
+        View
+}from './styles';
+
+export default function Home() {
+  return (
+    <StyledContainer>
+    <View> 
+
+            <StyledText>
+            <Text>Oiiii</Text>
+            </StyledText>  
+      </View>
+    </StyledContainer>
+  );
+}
